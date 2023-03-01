d2925f91a7fa03be6dc5810eb07ef7e1f7c0cd9f
Nolan Hargreaves <hargreavesnolan@gmail.com>
2023-02-28 18:59:42 -0600

    Nav bar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
+    ButtonModule,
     BrowserModule,
     AppRoutingModule
   ],
