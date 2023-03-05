95f243d074330a251b8ee2565a26512d88a02b75
Nolan Hargreaves <hargreavesnolan@gmail.com>
2023-03-05 14:08:48 -0600

    nav bar progress commit. + Jquery dependency

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  public onClickNavItem(linkId: string): void {
+    document?.getElementById(linkId)?.click()
+  }
 
 }
